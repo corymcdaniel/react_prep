3a0d49937aac0832cf7953cc787c33f70efed929
corymcdaniel <cmcdaniel@352media.com>
2017-02-11 15:50:07 -0500

    Update example

--- js/components/person-list.js
+++ js/components/person-list.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Person from './person';
 
-export default class PersonList extends React.Component {
-  render() {
-    const people = [];
-    for (let i=0; i<5; i++) {
-      people.push(<Person />);
-    }
-    return (
-      <div className="person-list">
-        {people}
-      </div>
-    );
-  }
+export default function PersonList() {
+  return (
+    <div className="person-list">
+      <Person name="Derek Zoolander"
+              imageUrl="https://scontent.cdninstagram.com/t51.2885-19/11377856_626372960798542_1396263462_a.jpg"
+              job="Male model" />
+      <Person name="Donald Knuth"
+              imageUrl="http://www-cs-faculty.stanford.edu/~uno/don.gif"
+              job="Clever chap" />
+    </div>
+  );
 }
