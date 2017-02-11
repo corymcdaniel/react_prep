2985f2649fe54ca4099c657c574778f95a8aa727
corymcdaniel <cmcdaniel@352media.com>
2017-02-11 15:18:15 -0500

    simple component overview

---
+++ js/components/person-list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Person from './person';
+
+export default class PersonList extends React.Component {
+  render() {
+    const people = [];
+    for (let i=0; i<5; i++) {
+      people.push(<Person />);
+    }
+    return (
+      <div className="person-list">
+        {people}
+      </div>
+    );
+  }
+}
